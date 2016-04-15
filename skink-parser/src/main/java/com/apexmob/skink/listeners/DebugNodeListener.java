d8b61763eaeff09d1a4f49bd4cca5c9a99489d5a
apexmob <apexmobility@gmail.com>
2016-04-14 21:58:38 -0500

    Add javadoc, Update to extend AbstractNodeListener

--- skink-parser/src/main/java/com/apexmob/skink/listeners/DebugNodeListener.java
+++ skink-parser/src/main/java/com/apexmob/skink/listeners/DebugNodeListener.java
@@ -4,22 +4,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.apexmob.skink.EndElement;
-import com.apexmob.skink.NodeListener;
 import com.apexmob.skink.StartElement;
 import com.apexmob.skink.Text;
 
-public class DebugNodeListener implements NodeListener {
+/**
+ * The DebugNodeListener logs the contents of all events at the debug level.
+ */
+public class DebugNodeListener extends AbstractNodeListener {
 	
 	private static final Logger logger = LoggerFactory.getLogger(DebugNodeListener.class);
 
+	/**
+	 * {@inheritDoc}
+	 * Log the contents of the event at debug level.
+	 */
 	public void onStartElement(StartElement start) {
 		logger.debug(start.getContent());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Log the contents of the event at debug level.
+	 */
 	public void onText(Text text) {
 		logger.debug(text.getContent());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Log the contents of the event at debug level.
+	 */
 	public void onEndElement(EndElement end) {
 		logger.debug(end.getContent());
 	}
