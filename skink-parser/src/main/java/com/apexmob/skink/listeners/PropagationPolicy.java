73ad616e7c47c4b7eba106987b9188d463a968a1
apexmob <apexmobility@gmail.com>
2016-04-14 22:49:49 -0500

    Add javadoc

--- skink-parser/src/main/java/com/apexmob/skink/listeners/PropagationPolicy.java
+++ skink-parser/src/main/java/com/apexmob/skink/listeners/PropagationPolicy.java
@@ -1,9 +1,24 @@
 package com.apexmob.skink.listeners;
 
+/**
+ * The PropagationPolicy enumeration is used to indicate what types of node events should be propagated to
+ * other listeners. 
+ */
 public enum PropagationPolicy {
 	
+	/**
+	 * Include all node events.
+	 */
 	All,
+	
+	/**
+	 * Include only tags.
+	 */
 	TagsOnly,
+	
+	/**
+	 * Include only the parent tags.
+	 */
 	ParentTagOnly;
 
 }
