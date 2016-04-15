5c302132b1041e608a3721aebb47527be2b60880
apexmob <apexmobility@gmail.com>
2016-04-14 21:58:17 -0500

    Add Javadoc

--- skink-parser/src/main/java/com/apexmob/skink/listeners/AbstractNodeListener.java
+++ skink-parser/src/main/java/com/apexmob/skink/listeners/AbstractNodeListener.java
@@ -5,20 +5,37 @@ import com.apexmob.skink.NodeListener;
 import com.apexmob.skink.StartElement;
 import com.apexmob.skink.Text;
 
+/**
+ * The AbstractNodeListener is an empty implementation of the NodeListener interface. It is
+ * recommended that all listeners extend it, overriding methods as necessary, to ensure forward
+ * compatibility with handling any new node events that might be added.
+ */
 public abstract class AbstractNodeListener implements NodeListener {
 	
+	/**
+	 * Construct a new instance.
+	 */
 	protected AbstractNodeListener() {
 		
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void onStartElement(StartElement start) {
 		//do nothing
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void onText(Text text) {
 		//do nothing
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void onEndElement(EndElement end) {
 		//do nothing
 	}
